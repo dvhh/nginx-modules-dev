abef55ce34231bef565b7891a2f92f3dd874ec87
dvhh <dvhh@yahoo.com>
2024-02-12 11:15:24 +0900

    alternate buffer approach

---
+++ buffer2/src/ngx-http-buffer.c
@@ -0,0 +1,310 @@
+#include "ngx-http-buffer.h"
+#include <stdbool.h>
+
+#define BUFFERED 0x08
+
+static void *
+create_conf(ngx_conf_t *config)
+{
+    ngx_log_error(
+        NGX_LOG_WARN,
+        config->log,
+        0,
+        __PRETTY_FUNCTION__
+    );
+    config_t *result;
+    result = ngx_pcalloc(config->pool, sizeof(config_t));
+    if(result == NULL) {return NULL;}
+    result->enabled = NGX_CONF_UNSET;
+
+    return result;
+}
+
+static char*
+merge_conf(ngx_conf_t *config, void* parent, void* child)
+{
+	const config_t *prev = parent;
+	config_t *conf = child;
+
+	ngx_conf_merge_value(conf->enabled, prev->enabled, 0);
+
+	return NGX_CONF_OK;
+}
+
+static ngx_http_output_header_filter_pt  ngx_http_next_header_filter;
+static ngx_http_output_body_filter_pt    ngx_http_next_body_filter;
+
+static ngx_int_t
+module_init(ngx_conf_t *config)
+{
+    ngx_log_error(
+        NGX_LOG_WARN,
+        config->log,
+        0,
+        __PRETTY_FUNCTION__
+    );
+    ngx_http_next_header_filter = ngx_http_top_header_filter;
+    ngx_http_top_header_filter = header_filter;
+
+    ngx_http_next_body_filter = ngx_http_top_body_filter;
+    ngx_http_top_body_filter = body_filter;
+
+    return NGX_OK;
+}
+
+static ngx_int_t
+header_filter(ngx_http_request_t *request)
+{
+    ngx_log_error(
+        NGX_LOG_WARN,
+        request->connection->log,
+        0,
+        __PRETTY_FUNCTION__
+    );
+    const config_t *config = ngx_http_get_module_loc_conf(
+        request, ngx_http_buffer_module
+    );
+    if(config == NULL) {return NGX_ERROR;}
+    if(! config->enabled) {
+        ngx_log_error(
+            NGX_LOG_WARN,
+            request->connection->log,
+            0,
+            "disabled"
+        );
+        return ngx_http_next_header_filter(request);
+    }
+
+    context_t *ctx = ngx_pcalloc(request->pool, sizeof(context_t));
+
+    if(ctx == NULL) {
+            ngx_log_error(
+                NGX_LOG_ERR,
+                request->connection->log,
+                0,
+                "could not allocate context"
+            );
+            return NGX_ERROR;
+    }
+    ngx_http_set_ctx(request, ctx, ngx_http_buffer_module);
+    return NGX_OK;
+}
+
+static bool
+add_buffer_piece(ngx_http_request_t *request, context_t *ctx, ngx_chain_t *in)
+{
+    for(ngx_chain_t *cl = in; cl != NULL; cl = cl->next) {
+        buffer_piece_t *current = ngx_pcalloc(request->pool, sizeof(buffer_piece_t));
+        if(current == NULL) {return false;}
+
+        ngx_buf_t *b = cl->buf;
+        if(b->in_file) {
+            current->length = b->file_last - b->file_pos;
+        }else{
+            current->length = b->last - b->pos;
+        }
+
+        current->buffer = ngx_palloc(request->pool, current->length);
+        if(current->buffer == NULL) {return false;}
+        if(b->in_file) {
+            ssize_t length = ngx_read_file(b->file, current->buffer, current->length, b->file_pos);
+            if(length == -1) {return false;}
+            if(length != current->length) {return false;}
+        }else{
+            unsigned char *end = ngx_cpymem(current->buffer, b->pos, current->length);
+            if(end == NULL) {return false;}
+            if((end - current->buffer) != current->length) {return false;}
+        }
+        if(ctx->last != NULL) {
+            ctx->last->next = current;
+        }
+        ctx->last = current;
+        if(ctx->head == NULL) {
+            ctx->head = current;
+        }
+        if(b->last_buf) {
+            return true;
+        }
+    } 
+    return true;
+}
+
+static ngx_int_t
+body_filter(ngx_http_request_t *request, ngx_chain_t *in)
+{
+    ngx_log_error(
+        NGX_LOG_WARN,
+        request->connection->log,
+        0,
+        __PRETTY_FUNCTION__
+    );
+    ngx_log_error(
+        NGX_LOG_WARN,
+        request->connection->log,
+        0,
+        "%p %p",
+        request,
+        in
+    );
+    
+    if(in == NULL) {
+        ngx_log_error(
+            NGX_LOG_WARN,
+            request->connection->log,
+            0,
+            "skip in"
+        );
+     
+        request->connection->buffered |= BUFFERED;
+        request->buffered |= BUFFERED;
+
+        return ngx_http_next_body_filter(request, in);
+        //return NGX_OK;
+    }
+    context_t *ctx = ngx_http_get_module_ctx(
+            request,
+            ngx_http_buffer_module
+        );
+    if(ctx == NULL) {
+        ngx_log_error(
+            NGX_LOG_WARN,
+            request->connection->log,
+            0,
+            "skip ctx"
+        );
+        return ngx_http_next_body_filter(request, in);
+    }
+    if (!add_buffer_piece(request, ctx, in)) {
+        ngx_log_error(
+            NGX_LOG_ERR,
+            request->connection->log,
+            0,
+            "could not copy buffer_chain"
+    	);
+	    return NGX_ERROR;
+    }
+
+    // check for last buffer
+    bool last = false;
+    size_t size = 0;
+    ngx_chain_t *cl = in;
+    while(cl != NULL) {
+        ngx_buf_t *b = cl->buf;
+        size_t current = 0;
+        if(b->in_file) {
+            current = b->file_last - b->file_pos;
+        }else{
+            current = b->last - b->pos;
+            //set buffer to zero length
+            //b->pos = b->last; 
+            b->last = b->pos;
+        }
+        size += current;
+        ngx_chain_t *old = cl;
+        cl = cl->next;
+        ngx_free_chain(request->pool, old);
+
+        if(b->last_buf) {
+            last = true;
+        }
+    }
+    size = 0;
+    for(buffer_piece_t *i = ctx->head;i != NULL; i = i->next) {
+        size += i->length;
+    }
+    ngx_log_error(
+        NGX_LOG_WARN,
+        request->connection->log,
+        0,
+        "last = %i",
+        last
+    );
+    if(last) {
+        request->connection->buffered &= ~BUFFERED;
+        return send_response(request, ctx);
+    }
+    ngx_log_error(
+        NGX_LOG_WARN,
+        request->connection->log,
+        0,
+        "waiting remain of file %z",
+        size
+    );
+    request->connection->buffered |= BUFFERED;
+    request->buffered |= BUFFERED;
+
+    return NGX_OK;
+}
+
+
+static
+ngx_int_t send_response(ngx_http_request_t *request, const context_t *ctx)
+{
+    ngx_log_error(
+        NGX_LOG_WARN,
+        request->connection->log,
+        0,
+        "%s:%i:%s",
+        __FILE__,
+        __LINE__,
+        __PRETTY_FUNCTION__
+    );
+    ngx_int_t result = ngx_http_next_header_filter(request);
+    if(result == NGX_ERROR) {
+        return NGX_ERROR;
+    }
+    if(request->header_only) {
+        return NGX_OK;
+    }
+    size_t size = 0;
+    for(buffer_piece_t *b = ctx->head; b != NULL; b = b->next) {
+        size += b->length;
+    }
+    ngx_log_error(
+        NGX_LOG_WARN,
+        request->connection->log,
+        0,
+        "buffer size %z",
+        size
+    );
+    ngx_buf_t *buf = ngx_pcalloc(request->pool, sizeof(ngx_buf_t));
+    //ngx_memzero(&buf, sizeof(buf));
+    buf->pos      = ngx_palloc(request->pool, size);
+    if(buf->pos == NULL){
+        return NGX_ERROR;
+    }
+    buf->start         = buf->pos;
+    buf->end           = buf->pos + size;
+    buf->last          = buf->pos;
+    buf->memory        = true;
+    buf->last_buf      = true;
+
+    ngx_log_error(
+        NGX_LOG_WARN,
+        request->connection->log,
+        0,
+        "gathering buffers",
+        size
+    );
+    for(buffer_piece_t *b = ctx->head; b != NULL; b = b->next) {
+        buf->last = ngx_cpymem(buf->last, b->buffer, b->length);
+        if(buf->last == NULL) {
+            return NGX_ERROR;
+        }
+    }
+   ngx_log_error(
+        NGX_LOG_WARN,
+        request->connection->log,
+        0,
+        "done %z / %z",
+        buf->last - buf->pos,
+        size
+    );
+
+    ngx_chain_t out;
+    out.buf  = buf;
+    out.next = NULL;
+
+    //return NGX_OK;
+    return ngx_http_next_body_filter(request, &out);
+}
---
+++ buffer2/src/ngx-http-buffer.h
@@ -0,0 +1,114 @@
+#pragma once
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+
+#ifndef _NGX_HTB_H_
+#define _NGX_HTB_H_
+
+/**
+ * Configuration
+ **/
+typedef struct {
+    /// enable buffer flag
+    ngx_flag_t enabled;
+} config_t;
+
+
+typedef struct buffer_piece {
+    struct buffer_piece *next;
+    unsigned char *buffer;
+    ssize_t length;
+} buffer_piece_t;
+
+/**
+ * Request context
+ **/
+typedef struct {
+    buffer_piece_t *head;
+    buffer_piece_t *last;
+} context_t;
+
+/**
+ * Command 
+ **/
+static ngx_command_t  module_commands[] = {
+    {
+        /// enable buffer
+        ngx_string("buffer_response"),
+        NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_FLAG,
+        ngx_conf_set_flag_slot,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(config_t, enabled),
+        NULL
+    },
+    ngx_null_command
+};
+
+/**
+ * Module initialization
+ *
+ * This will be used for rerouting the nginx filter routing
+ **/
+static ngx_int_t module_init(ngx_conf_t *config);
+
+/**
+ * Header filter
+ *
+ * This filter function is called after nginx has received and parsed the response headers from upstream
+ **/
+static ngx_int_t header_filter(ngx_http_request_t *request);
+
+/**
+ * Body filter
+ *
+ * This filter function is called each time after nginx has received a chunk of the response body
+ **/
+static ngx_int_t body_filter(ngx_http_request_t *request, ngx_chain_t *in);
+
+/**
+ * Allocate and initialize config
+ **/
+static void* create_conf(ngx_conf_t *config);
+
+/**
+ * Merge config
+ */
+static char *merge_conf(ngx_conf_t *config, void *parent, void *child);
+
+/**
+ * Send response header and body
+ **/
+static ngx_int_t send_response(ngx_http_request_t *request, const context_t *ctx);
+
+
+static ngx_http_module_t  module_ctx = {
+    NULL,           /* preconfiguration */
+    module_init,    /* postconfiguration */
+
+    NULL,           /* create main configuration */
+    NULL,           /* init main configuration */
+
+    NULL,           /* create server configuration */
+    NULL,           /* merge server configuration */
+
+    create_conf,    /* create location configuration */
+    merge_conf      /* merge location configuration */
+};
+
+ngx_module_t  ngx_http_buffer_module = {
+    NGX_MODULE_V1,
+    &module_ctx,        /* module context */
+    module_commands,    /* module directives */
+    NGX_HTTP_MODULE,    /* module type */
+    NULL,               /* init master */
+    NULL,               /* init module */
+    NULL,               /* init process */
+    NULL,               /* init thread */
+    NULL,               /* exit thread */
+    NULL,               /* exit process */
+    NULL,               /* exit master */
+    NGX_MODULE_V1_PADDING
+};
+
+#endif // _NGX_HTB_H_
